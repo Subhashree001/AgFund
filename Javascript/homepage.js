23e60ad38d4ecdb4632b3d214dc8a06644db4476
Subhashree B <74830950+Subhashree001@users.noreply.github.com>
2021-05-17 16:37:04 +0530

    Add files via upload

---
+++ Javascript/homepage.js
@@ -0,0 +1,12 @@
+ //a//lert("hello");
+
+var myVar;
+
+// function myFunction() {
+//   myVar = setTimeout(showPage, 3);
+// }
+//
+// function showPage() {
+//   document.getElementById("loader").style.display = "none";
+//   document.getElementById("myDiv").style.display = "block";
+// }
